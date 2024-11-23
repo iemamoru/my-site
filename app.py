f341443eee0f04d58821463e8ff88967a52e4b0e
iemamoru <josemendoxe@gmail.com>
2024-11-24 00:48:20 +0900

    ipaddress

--- app.py
+++ app.py
@@ -19,16 +19,6 @@ allowed_pages = {
     "rinapen": False,
 }
 
-def get_user_ip():
-    # リバースプロキシ環境では'X-Forwarded-For'を確認
-    if request.headers.get('X-Forwarded-For'):
-        ip = request.headers['X-Forwarded-For'].split(',')[0]  # 複数のIPが含まれる場合は最初のものを取得
-    else:
-        ip = request.remote_addr or '8.8.8.8'  # ローカル環境ではデフォルトでGoogleのDNSを使用
-    # ローカルホストのIPをテスト用IPに置き換え
-    if ip == '127.0.0.1':
-        ip = '8.8.8.8'
-    return ip
 
 # Helper function to determine language based on geolocation
 def get_user_language(ip):
@@ -46,7 +36,10 @@ def get_user_language(ip):
 # Redirect root URL to language-specific home based on location
 @app.route('/')
 def index():
-    user_ip = get_user_ip()
+    if request.headers.getlist("X-Forwarded-For"):
+        user_ip = request.headers.getlist("X-Forwarded-For")[0]
+else:
+        user_ip = request.remote_addr
     user_language = get_user_language(user_ip)
     return redirect(f'/{user_language}/home')
 
